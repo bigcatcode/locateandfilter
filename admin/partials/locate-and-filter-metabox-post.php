944427b7599bbc50917dc0ed7c7cf50a81e32f94
dgamoni <dgamoni@gmail.com>
2019-09-22 11:08:28 +0300

    fix load options on metabox-post

--- admin/partials/locate-and-filter-metabox-post.php
+++ admin/partials/locate-and-filter-metabox-post.php
@@ -1,8 +1,11 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
 <?php wp_nonce_field ('I961JpJQTj0crLKH0mGB', 'locate_anything_class_nonce' );
-$post_params = Locate_And_Filter_Admin::getUserMetas( $object->ID );
-// if($post_type=="user") $post_params=Locate_And_Filter_Admin::getUserMetas($object->ID);
-// else  $post_params=Locate_And_Filter_Admin::getPostMetas($object->ID);
+//$post_params = Locate_And_Filter_Admin::getUserMetas( $object->ID );
+if ( $post_type=="user" ) {
+	$post_params=Locate_And_Filter_Admin::getUserMetas($object->ID);
+} else {
+	$post_params=Locate_And_Filter_Admin::getPostMetas($object->ID);
+}  
  ?>
 <div id="locate-anything-wrapper-post">
 <h2 class="nav-tab-wrapper">
